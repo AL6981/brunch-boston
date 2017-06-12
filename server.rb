e0cedc3e1585e75fa436ea467445796021c3243b
Chrissy Fancy <chrissyfancy@gmail.com>
2017-06-12 14:04:41 -0400

    Create branch for students to code along

--- server.rb
+++ server.rb
@@ -6,34 +6,34 @@ require_relative 'app/restaurant.rb'
 set :bind,'0.0.0.0'  # bind to all interfaces,http://www.sinatrarb.com/configuration.html
 
 def array_of_restaurant_objects
-  restaurants = []
-  CSV.foreach('restaurants.csv', headers: true) do |row|
-    restaurants << Restaurant.new(row["id"], row["name"], row["address"], row["description"], row["url"], row["image"])
-  end
-  restaurants
+  #create an empty array
+  #loop through CSV file to grab data -> CSV.foreach('restaurants.csv', headers: true) do |row|
+    # use binding.pry to see what is passed each loop
+    #add new restaurant object to the empty array -> Restaurant.new(row["id"], row["name"], row["address"], row["description"], row["url"], row["image"])
+  #return array of objects
 end
 
 get '/' do
-  redirect to "/restaurants"
+  # redirect to "/restaurants"
 end
 
 get '/restaurants' do
-  @restaurants = array_of_restaurant_objects
-  erb :index
+  # Set variable to refer to array_of_restaurant_objects
+  # erb :index
 end
 
 get '/restaurant/:id' do
-  @restaurant = array_of_restaurant_objects.find { |r| r.id == params[:id]}
-  erb :show
+  # Use the params to view show page for the restaurant you click
+  # @restaurant = array_of_restaurant_objects.find { |r| r.id == params[:id]}
+  # erb :show
 end
 
 get "/restaurants/new" do
-  erb :new
+  # erb :new
 end
 
 post '/restaurants' do
-  CSV.open("restaurants.csv", "a") do |csv|
-    csv << [array_of_restaurant_objects.length + 1, params[:name], params[:address],"#{params[:description]}",params[:url],params[:image]]
-  end
-  redirect to "/"
+  # access the CSV file with CSV.open("restaurants.csv", "a") do |csv|
+    # push new restaurant params into csv -- note: has to be passed in as an array
+  # redirect to "/"
 end
