64cd7900a26a251cf9a267a6c5b8fc1bfe8d7520
[AL6981] <amylynnarrington@gmail.com>
2018-03-19 11:30:55 -0400

    Created a code-along version for students to work through

--- app/restaurant.rb
+++ app/restaurant.rb
@@ -1,12 +1,3 @@
-class Restaurant
-  attr_reader :id, :name, :address, :description, :url, :image
 
-  def initialize(id, name, address, description, url, image)
-    @id = id
-    @name = name
-    @address = address
-    @description = description
-    @url = url
-    @image = image
-  end
-end
+#create a Restaurant class
+  ## should take in: id, name, address, description, url, image
